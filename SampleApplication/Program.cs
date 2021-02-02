391feeb3106bcc78d85275fb1fe4cf14eed86e55
seangnewman <seangnewman@msn.com>
2021-02-01 19:28:36 -0700

    Creating IL code outside of the .NET environment using text editor

---
+++ SampleApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet(args[0]);
+        }
+
+        private static void Greet(string name)
+        {
+            Console.WriteLine($"Greetings {name}");
+        }
+    }
+}
